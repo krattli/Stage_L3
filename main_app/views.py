d804dc99037cb28f779332d35117384a45bcaccd
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-06-23 15:57:51 +0200

    création des apps principales

--- main_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Bonjour depuis l'app principale".encode("utf-8"))
---
+++ xAI_eval/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class XaiEvalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'xAI_eval'
