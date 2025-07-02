8c27d882d2da208443636d061c82016088a565f6
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-07-02 16:38:48 +0200

    modèles de machine learning fonctionnel sur les données d'entrainement

--- mlModels/aiModel.py
+++ mlModels/aiModel.py
@@ -4,19 +4,23 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
-from dataPreparation import prepData
+
+from mlModels.dataPreparation import prepData
+
+import os
+from django.conf import settings
 
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix
 
-WHITEBOX_MODELS = {
+BLACKBOX_MODELS = {
     "Decision Tree": DecisionTreeClassifier(),
     "Logistic Regression": LogisticRegression(),
     "Naive Bayes": GaussianNB(),
-    "KNN": KNeighborsClassifier()
+    "KNN": KNeighborsClassifier(),
 }
 
-BLACKBOX_MODELS = {
+WHITEBOX_MODELS = {
     "Random Forest": RandomForestClassifier(),
     "Bagging": BaggingClassifier(),
     "Extra Trees": ExtraTreesClassifier(),
@@ -24,30 +28,34 @@ BLACKBOX_MODELS = {
     "MLP": MLPClassifier()
 }
 
-def plotConfusionMatrix(model):
-    X_train, X_test, y_train, y_test = prepData('../static/data/dataset.csv')
-    # Entraînement du modèle
-    model.fit(X_train, y_train)
-    # Prédictions
-    y_pred = model.predict(X_test)
-    # Afficher les résultats
-    print("Classification Report:")
-    print(classification_report(y_test, y_pred))
-    print("Confusion Matrix:")
-    print(confusion_matrix(y_test, y_pred))
-    # On va afficher la matrice de confusion de façon graphique
-    plt.figure(figsize=(8, 6))
-    plt.title(f"Matrice de Confusion de :{type(model)}")
-    plt.imshow(confusion_matrix(y_test, y_pred), cmap='Blues', interpolation='nearest')
-    plt.colorbar()
-    plt.xlabel('Prédictions')
-    plt.ylabel('Réelles')
-    plt.xticks([0, 1, 2, 3], ['E+P+', 'E+P-', 'E-P+', 'E-P-'])
-    plt.yticks([0, 1, 2, 3], ['E+P+', 'E+P-', 'E-P+', 'E-P-'])
-    plt.show()
+AVAILABLE_MODELS = {**WHITEBOX_MODELS, **BLACKBOX_MODELS}
+
+"""
+    Prends un String du nom de modèle et montre la matrice de confusion que ce modèle produit sur les données d'entrainement statiques
+"""
+def plotConfusionMatrix(MLmodel):
+    if MLmodel in AVAILABLE_MODELS:
+        model = AVAILABLE_MODELS[MLmodel]
+        path = os.path.join(settings.BASE_DIR, 'static', 'data', 'dataset.csv')
+        X_train, X_test, y_train, y_test = prepData(path)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        print("Classification Report:")
+        print(classification_report(y_test, y_pred))
+        print("Confusion Matrix:")
+        print(confusion_matrix(y_test, y_pred))
+        plt.figure(figsize=(8, 6))
+        plt.title(f"Matrice de Confusion de : {MLmodel}")
+        plt.imshow(confusion_matrix(y_test, y_pred), cmap='Blues', interpolation='nearest')
+        plt.colorbar()
+        plt.xlabel('Prédictions')
+        plt.ylabel('Réelles')
+        plt.xticks([0, 1, 2, 3], ['E+P+', 'E+P-', 'E-P+', 'E-P-'])
+        plt.yticks([0, 1, 2, 3], ['E+P+', 'E+P-', 'E-P+', 'E-P-'])
+        plt.show()
+    else:
+        print(f"Le modèle d'IA '{MLmodel}' n'est pas encore disponible. Veuillez choisir parmi les modèles suivants : {list(AVAILABLE_MODELS.keys())}")
 
 if __name__ == "__main__":
-    for model in WHITEBOX_MODELS.values():
-        plotConfusionMatrix(model)
-    for model in BLACKBOX_MODELS.values():
+    for model in AVAILABLE_MODELS.values():
         plotConfusionMatrix(model)
--- mlModels/dataPreparation.py
+++ mlModels/dataPreparation.py
@@ -3,7 +3,9 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 
-CALCULATED_COLUMNS = [
+USELESS_COLUMNS = [
+    'ACADEMIC_Y',
+    'COURSE_NAME',
     'AVG_RATING',
     'ACTIVITY_ORIENTED_INVESTMENT',
     'INTERACTION_ORIENTED_INVESTMENT',
@@ -14,48 +16,69 @@ CALCULATED_COLUMNS = [
 
 def prepData(filepath):
     data = pd.read_csv(filepath, sep=';', on_bad_lines='warn')
-    dropUselessColumns(data)
-    handleTimeTypeVariables(data)
-    handleMissingData(data)
-    labelTarget(data)
-    features = data.drop('DIFFICULTY', axis=1)
-    target = data['DIFFICULTY']
+    data = dropUselessColumns(data)                # toutes les colonnes dont la valeur peux dépendre d'une autre colonne est supprimée pour éviter les redondances
+    data = handleTimeTypeVariables(data)           # les variables temporelles sont séparées en plusieurs autres colonnes (mois, jour, heure, minute)
+    data = labelTarget(data, 'PUBLIC')
+    data = labelTarget(data, 'DIFFICULTY')
+    data = cleanComas(data)                        # dans le csv, les nombres à virgule sont pas reconnus par python pour être converites en float, on doit donc les remplacer
+    data = handleMissingData(data)                 # les données manquantes sont remplaées par -1
+    #data = pd.get_dummies(data)
+    print(data.columns)
+    features, target = chooseTarget(data, 'DIFFICULTY')
     scaledFeatures = scaling(features)
     X_res, y_res = equilibrate(scaledFeatures, target)
     X_train, X_test, y_train, y_test = train_test_split(X_res, y_res, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
 def dropUselessColumns(data):
-    # toutes les colonnes dont la valeur peux dépendre d'une autre colonne est supprimée pour éviter les redondances
-    data.drop(columns=CALCULATED_COLUMNS, inplace=True)
-    return
+    data.drop(columns=USELESS_COLUMNS, inplace=True)
+    return data
 
 def handleTimeTypeVariables(data):
     new_columns = []
     for col in data.columns:
-        if 'SUBMISSION' in col or 'ACCESS' in col:
-            day_col = data[col].dt.day
-            month_col = data[col].dt.month
-            hour_col = data[col].dt.hour
-            minute_col = data[col].dt.minute
-            new_columns.append(day_col.rename(f"{col}_day"))
-            new_columns.append(month_col.rename(f"{col}_month"))
-            new_columns.append(hour_col.rename(f"{col}_hour"))
-            new_columns.append(minute_col.rename(f"{col}_minute"))
-            data.drop(columns=[col], inplace=True)
+        if 'FIRST' in col or 'LAST' in col:
+            data[col] = data[col].str.strip()
+            data[col] = data[col].str.replace(r'[-]', '', regex=True)
+            try:
+                data[col] = pd.to_datetime(data[col], errors='coerce')
+                if pd.api.types.is_datetime64_any_dtype(data[col]):
+                    day_col = data[col].dt.day
+                    month_col = data[col].dt.month
+                    hour_col = data[col].dt.hour
+                    minute_col = data[col].dt.minute
+                    new_columns.append(day_col.rename(f"{col}_day"))
+                    new_columns.append(month_col.rename(f"{col}_month"))
+                    new_columns.append(hour_col.rename(f"{col}_hour"))
+                    new_columns.append(minute_col.rename(f"{col}_minute"))
+                    data.drop(columns=[col], inplace=True)
+            except Exception as e:
+                print(f"Erreur lors de la conversion de {col}: {e}")
     data = pd.concat([data] + new_columns, axis=1)
-    return
+    return data
 
 
 def handleMissingData(data):
     data.fillna(-1, inplace=True)
-    return
+    return data
 
-def labelTarget(data):
+def labelTarget(data, targetLabel):
     label_enc = LabelEncoder()
-    data['DIFFICULTY'] = label_enc.fit_transform(data['DIFFICULTY'])
-    data = pd.get_dummies(data)
-    return
+    data[targetLabel] = data[targetLabel].str.strip()
+    data[targetLabel] = label_enc.fit_transform(data[targetLabel])
+    return data
+
+def cleanComas(data):
+    for col in data.columns:
+        if data[col].dtype == 'object':
+            data[col] = data[col].str.replace(',', '.', regex=True)
+    return data
+
+def chooseTarget(data, targetLabel):
+    features = data.drop(targetLabel, axis=1)
+    target = data[targetLabel]
+    return features, target
+
 
 def scaling(data):
     scaler = MinMaxScaler()
@@ -67,5 +90,5 @@ def normalize(data):
 
 def equilibrate(X, y):
     smote = SMOTE(random_state=42)
-    X_res, y_res = smote.fit_resample(X, y)
-    return X_res, y_res
+    result = smote.fit_resample(X, y)
+    return result[0], result[1]
